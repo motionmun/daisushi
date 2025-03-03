111d93ed559da00c28489e6871159babd7b05123
Движение Мун <134210326+motionmun@users.noreply.github.com>
2025-03-04 05:53:16 +0900

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,14 @@
 from aiogram import Router, types
 from aiogram.filters import Command
 from database import add_user
+from keyboards.menu_kb import menu_kb  # Импорт клавиатуры
 
 router = Router()
 
 @router.message(Command("start"))
 async def start_command(message: types.Message):
     add_user(message.from_user.id, message.from_user.full_name)
-    await message.answer(f"Привет, {message.from_user.full_name}! Добро пожаловать в Dai Sushi 🍣\nВыберите действие в меню.")
+    await message.answer(
+        f"Привет, {message.from_user.full_name}! Добро пожаловать в Dai Sushi 🍣\nВыберите действие в меню:",
+        reply_markup=menu_kb  # Показываем кнопки пользователю
+    )
