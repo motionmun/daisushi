d1ff0c1e256877ca339edb478684d39d77fa98b9
Движение Мун <134210326+motionmun@users.noreply.github.com>
2025-03-04 06:14:14 +0900

    Update menu_models.py

--- models/menu_models.py
+++ models/menu_models.py
@@ -1,1 +1,8 @@
+class MenuItem:
+    def __init__(self, id, name, description, price, photo):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.price = price
+        self.photo = photo
 
