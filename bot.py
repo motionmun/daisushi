42f90ee5fca0a7c9efa5faeacc1f101e974566bb
Движение Мун <134210326+motionmun@users.noreply.github.com>
2025-03-04 04:39:38 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,10 +35,11 @@ async def show_cart(message: types.Message):
 
 # === Запуск бота ===
 async def main():
-    dp.include_router(router)  # Подключаем Router
-    await dp.start_polling(bot)
+    try:
+        dp.include_router(router)
+        await dp.start_polling(bot)
+    except Exception as e:
+        print(f"Ошибка: {e}")
 
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(main())
+    asyncio.run(main())
