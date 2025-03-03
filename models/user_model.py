8efe196ed29df92e38c90f496c4287c96ccd75b0
Движение Мун <134210326+motionmun@users.noreply.github.com>
2025-03-04 06:15:01 +0900

    Add files via upload

---
+++ models/user_model.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, id, telegram_id, name, orders_count):
+        self.id = id
+        self.telegram_id = telegram_id
+        self.name = name
+        self.orders_count = orders_count
