81f60a40d174d319a6cdeb6d980032355281d1a7
Движение Мун <134210326+motionmun@users.noreply.github.com>
2025-03-04 07:28:59 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,25 @@
 import asyncio
-from models.order_model import setup_db
-setup_db()
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from database import create_tables
-from handlers import start, menu, cart, admin
+from handlers import start, menu, cart, order, admin
+
+# Создаем таблицы в БД (если их нет)
+create_tables()
+
+# Создаем бота и диспетчер
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+# Подключаем хендлеры
+dp.include_router(start.router)
+dp.include_router(menu.router)
+dp.include_router(cart.router)
+dp.include_router(order.router)
+dp.include_router(admin.router)
 
 async def main():
-    create_tables()
-
-    bot = Bot(token=TOKEN)
-    dp = Dispatcher()
-
-    dp.include_router(start.router)
-    dp.include_router(menu.router)
-    dp.include_router(cart.router)
-    dp.include_router(admin.router)
-
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
+if name == "__main__":
     asyncio.run(main())
